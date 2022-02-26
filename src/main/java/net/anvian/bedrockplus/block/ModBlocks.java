7576a68b50672b8c7064b877e47d0d162af401a1
anviaan <andre.carbajal.vargas@gmail.com>
2022-02-25 22:30:17 -0500

    changed some settings

--- src/main/java/net/anvian/bedrockplus/block/ModBlocks.java
+++ src/main/java/net/anvian/bedrockplus/block/ModBlocks.java
@@ -8,14 +8,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
     public static final Block IMPURE_BEDROCK = registerBlock("impure_bedrock",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE)
-                    .strength(250.0f,6000.0f).requiresTool()));
+            new Block(FabricBlockSettings.copyOf(Blocks.ANCIENT_DEBRIS)
+                    .sounds(BlockSoundGroup.DEEPSLATE)
+                    .strength(250.0f,6000.0f)
+                    .requiresTool()));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
