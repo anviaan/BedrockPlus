9c2af2850d29106db87847d3ddf835a3c0e344f5
anviaan <andre.carbajal.vargas@gmail.com>
2022-03-14 22:00:01 -0500

    1.18.2 updated

--- src/main/java/net/anvian/bedrockplus/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/anvian/bedrockplus/world/feature/ModConfiguredFeatures.java
@@ -2,27 +2,29 @@ package net.anvian.bedrockplus.world.feature;
 
 import net.anvian.bedrockplus.BedrockPlusMod;
 import net.anvian.bedrockplus.block.ModBlocks;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class ModConfiguredFeatures {
-    public static final List<OreFeatureConfig.Target> OVERWORLD_IMPURE_BEDROCK_ORES = List.of(
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.IMPURE_BEDROCK.getDefaultState()));
+    public static final ConfiguredFeature<?,?> OVERWORLD_IMPURE_BEDROCK_ORE_CONFIG = new ConfiguredFeature(
+            Feature.ORE, new OreFeatureConfig(
+            OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
+            ModBlocks.IMPURE_BEDROCK.getDefaultState(),10));
 
-    public static final ConfiguredFeature<?, ?> IMPURE_BEDROCK_ORE = register("impure_bedrock_ore",
-            //maximum amount shown together
-            Feature.ORE.configure(new OreFeatureConfig(OVERWORLD_IMPURE_BEDROCK_ORES, 5)));
-
-
-
-    public static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String name, ConfiguredFeature<FC, ?> configuredFeature) {
-        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(BedrockPlusMod.MOD_ID, name),
-                configuredFeature);
-    }
+    public static PlacedFeature OVERWORLD_IMPURE_BEDROCK_ORE_PLACED = new PlacedFeature(
+            RegistryEntry.of(OVERWORLD_IMPURE_BEDROCK_ORE_CONFIG),
+            Arrays.asList(
+                    CountPlacementModifier.of(30), // number of veins per chunk
+                    SquarePlacementModifier.of(),
+                    HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(-58))
+            )
+    );
 
     public static void registerConfiguredFeatures() {
         System.out.println("Registering ModConfiguredFeatures for " + BedrockPlusMod.MOD_ID);
--- src/main/java/net/anvian/bedrockplus/world/gen/ModOreGeneration.java
+++ src/main/java/net/anvian/bedrockplus/world/gen/ModOreGeneration.java
@@ -1,13 +1,25 @@
 package net.anvian.bedrockplus.world.gen;
 
-import net.anvian.bedrockplus.world.feature.ModPlacedFeatures;
+import net.anvian.bedrockplus.BedrockPlusMod;
+import net.anvian.bedrockplus.world.feature.ModConfiguredFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
 
 public class ModOreGeneration {
     public static void generateOres() {
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
-                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.IMPURE_BEDROCK_ORE_PLACED_KEY);
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
+                new Identifier(BedrockPlusMod.MOD_ID, "overworld_impure_bedrock_ore"), ModConfiguredFeatures.OVERWORLD_IMPURE_BEDROCK_ORE_CONFIG);
+
+        Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(BedrockPlusMod.MOD_ID, "overworld_impure_bedrock_ore"),
+                ModConfiguredFeatures.OVERWORLD_IMPURE_BEDROCK_ORE_PLACED);
+
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES,
+                RegistryKey.of(Registry.PLACED_FEATURE_KEY,
+                        new Identifier(BedrockPlusMod.MOD_ID, "overworld_impure_bedrock_ore")));
     }
 }
