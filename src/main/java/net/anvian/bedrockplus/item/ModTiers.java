275f9729090b78c5c4c705c390f357faab879edb
anvian <andre.carbajal.vargas@gmail.com>
2022-07-14 22:13:54 -0500

    modify ModTiers

--- src/main/java/net/anvian/bedrockplus/item/ModTiers.java
+++ src/main/java/net/anvian/bedrockplus/item/ModTiers.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier INSTANCE = new ForgeTier(2,1400,1.5f,
-            2f,22, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier INSTANCE = new ForgeTier(4,4062,10.0f,
+            5f,30, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.IMPURE_BEDROCK_INGOT.get()));
 }
