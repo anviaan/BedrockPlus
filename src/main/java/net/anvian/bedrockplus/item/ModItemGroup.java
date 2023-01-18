2ca2fd0906a6d635d15e35eff5517127e982607b
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-18 13:26:42 -0500

    fix item group

--- src/main/java/net/anvian/bedrockplus/item/ModItemGroup.java
+++ src/main/java/net/anvian/bedrockplus/item/ModItemGroup.java
@@ -1,13 +1,19 @@
 package net.anvian.bedrockplus.item;
 
 import net.anvian.bedrockplus.BedrockPlusMod;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup BEDROCKPLUS = FabricItemGroupBuilder.build
-            (new Identifier(BedrockPlusMod.MOD_ID, "impure_bedrock"),
-                    () -> new ItemStack(ModItems.IMPURE_BEDROCK_INGOT));
+    public static ItemGroup MANGO;
+
+    public static void registerItemGroup(){
+        MANGO = FabricItemGroup.builder(new Identifier(BedrockPlusMod.MOD_ID, "bedrockplus_item_group"))
+                .displayName(Text.translatable("itemGroup.bedrockplus.impure_bedrock"))
+                .icon(()-> new ItemStack(ModItems.IMPURE_BEDROCK_INGOT))
+                .build();
+    }
 }
