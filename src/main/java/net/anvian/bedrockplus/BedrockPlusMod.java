86fe8e474c1bef241b1aac85835970d6b6847438
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-18 13:40:36 -0500

    add item froup to main class

--- src/main/java/net/anvian/bedrockplus/BedrockPlusMod.java
+++ src/main/java/net/anvian/bedrockplus/BedrockPlusMod.java
@@ -1,21 +1,24 @@
 package net.anvian.bedrockplus;
 
 import net.anvian.bedrockplus.block.ModBlocks;
-import net.anvian.bedrockplus.item.ModItems;
 import net.anvian.bedrockplus.config.ModConfigs;
+import net.anvian.bedrockplus.item.ModItemGroup;
+import net.anvian.bedrockplus.item.ModItems;
 import net.anvian.bedrockplus.world.feature.ModConfiguredFeatures;
-import net.anvian.bedrockplus.world.gen.ModWorldGen;
 import net.fabricmc.api.ModInitializer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BedrockPlusMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	public static final String MOD_ID = "bedrockplus";
+	public static final String MOD_ID ="bedrockplus";
+	public static final Logger LOGGER = LoggerFactory.getLogger("bedrockplus");
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+
+		ModItemGroup.registerItemGroup();
 
 		ModConfiguredFeatures.registerConfiguredFeatures();
 
@@ -25,6 +28,5 @@ public class BedrockPlusMod implements ModInitializer {
 
 		ModBlocks.registerModBlock();
 
-		ModWorldGen.generateWorldGen();
 	}
 }
