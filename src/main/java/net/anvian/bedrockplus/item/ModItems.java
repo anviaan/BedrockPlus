49d0d6e19801005e58a1d69ff3e5f707dc82c952
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-01-18 12:22:11 -0500

    Revert "fix items and blocks 1.19.3"
    This reverts commit 66589fd048795259331fd36b0277daac2f8f4fdf.

--- src/main/java/net/anvian/bedrockplus/item/ModItems.java
+++ src/main/java/net/anvian/bedrockplus/item/ModItems.java
@@ -11,57 +11,56 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.SwordItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item IMPURE_BEDROCK_INGOT = registerItem("impure_bedrock_ingot",
-            new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).fireproof()));
+            new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).group(ModItemGroup.BEDROCKPLUS).fireproof()));
 
     public static final Item IMPURE_BEDROCK_SCRAP = registerItem("impure_bedrock_scrap",
-            new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).fireproof()));
+            new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON).group(ModItemGroup.BEDROCKPLUS).fireproof()));
 
     //armor
     public static final Item IMPURE_BEDROCK_HELMET = registerItem("impurebedrock_helmet",
             new ModArmorItem(ModArmorMaterials.IMPUREBEDROCK, EquipmentSlot.HEAD,
-                    new FabricItemSettings().fireproof()));
+                    new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPURE_BEDROCK_CHESTPLATE = registerItem("impurebedrock_chestplate",
             new ModArmorItem(ModArmorMaterials.IMPUREBEDROCK, EquipmentSlot.CHEST,
-                    new FabricItemSettings().fireproof()));
+                    new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPURE_BEDROCK_LEGGINGS = registerItem("impurebedrock_leggings",
             new ModArmorItem(ModArmorMaterials.IMPUREBEDROCK, EquipmentSlot.LEGS,
-                    new FabricItemSettings().fireproof()));
+                    new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPURE_BEDROCK_BOOTS = registerItem("impurebedrock_boots",
             new ModArmorItem(ModArmorMaterials.IMPUREBEDROCK, EquipmentSlot.FEET,
-                    new FabricItemSettings().fireproof()));
+                    new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
 
     //tools
     public static final Item IMPUREBEDROCK_SWORD = registerItem("impurebedrock_sword",
             new SwordItem(ModToolMaterials.INSTANCE,
                     ModConfigs.SwordAttackDamage,(float) ModConfigs.SwordAttackSpeed,
-                        new FabricItemSettings().fireproof()));
+                        new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPUREBEDROCK_PICKAXE = registerItem("impurebedrock_pickaxe",
             new ModPickaxeItem(ModToolMaterials.INSTANCE,
                     ModConfigs.PickaxeAttackDamage,(float)ModConfigs.PickaxeAttackSpeed,
-                        new FabricItemSettings().fireproof()));
+                        new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPUREBEDROCK_AXE = registerItem("impurebedrock_axe",
             new ModAxeItem(ModToolMaterials.INSTANCE,
                     ModConfigs.AxeAttackDamage, (float)ModConfigs.AxeAttackSpeed,
-                        new FabricItemSettings().fireproof()));
+                        new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPUREBEDROCK_SHOVEL = registerItem("impurebedrock_shovel",
             new ShovelItem(ModToolMaterials.INSTANCE,
                     (float)ModConfigs.ShovelAttackDamage,(float)ModConfigs.ShovelAttackSpeed,
-                        new FabricItemSettings().fireproof()));
+                        new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
     public static final Item IMPUREBEDROCK_HOE = registerItem("impurebedrock_hoe",
             new ModHoeItem(ModToolMaterials.INSTANCE,
                     ModConfigs.HoeAttackDamage,(float)ModConfigs.HoeAttackSpeed,
-                        new FabricItemSettings().fireproof()));
+                        new FabricItemSettings().group(ModItemGroup.BEDROCKPLUS).fireproof()));
 
     private static Item registerItem(String name,Item item){
-        return Registry.register(Registries.ITEM, new Identifier(BedrockPlusMod.MOD_ID, name), item);
+        return Registry.register(Registry.ITEM, new Identifier(BedrockPlusMod.MOD_ID, name), item);
     }
 
     public static void registerModItems(){
