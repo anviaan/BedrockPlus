55f04a57dc40ec6d5a64eaeefeec502f26b928b7
Andre Carbajal <andre.carbajal.vargas@gmail.com>
2023-02-01 19:33:15 -0500

    adjustments to world gen

--- src/main/java/net/anvian/bedrockplus/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/anvian/bedrockplus/world/feature/ModPlacedFeatures.java
@@ -21,7 +21,7 @@ public class ModPlacedFeatures {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
         register(context, IMPUREBEDROCK_ORE_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.IMPURE_BEDROCK_ORE_KEY),
-                commonOrePlacement(3, // VeinsPerChunk
+                commonOrePlacement(4, // VeinsPerChunk
                         HeightRangePlacement.triangle(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(-56))));
     }
 
